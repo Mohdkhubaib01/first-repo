930cc4e1826507525fc801dfdb979c1768a10e7e
MOhdKhubaib <mkhubaib543@gmail.com>
2025-05-28 12:22:26 +0530

    add fiel

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("a = ",a)
+var a = 10
+console.log("a = ",a)
+// console.log("b : ",b)
+let b = 10
+console.log("b : ",b)
+b = 20;
+console.log("b : ",b)
+var a = 40;
+console.log("a : ",a)
+const c = 10
+console.log("c : ",c)
+c = 20;
+console.log("c : ",c)
